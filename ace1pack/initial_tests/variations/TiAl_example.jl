30b0a6e3b770a4a5e3632428cf698067e130e1ea
Spencer Wyant <17836774+swyant@users.noreply.github.com>
2023-07-10 10:32:23 -0400

    some more ACE fitting experiments

---
+++ ace1pack/initial_tests/variations/TiAl_example.jl
@@ -0,0 +1,21 @@
+using ACE1pack 
+
+data_file = joinpath(ACE1pack.artifact("TiAl_tutorial"), "TiAl_tutorial.xyz")
+
+data = read_extxyz(data_file)
+
+model = acemodel(elements = [:Ti, :Al],
+                order = 3,
+                totaldegree = 6,
+                rcut = 5.5,
+                Eref = [:Ti => -1586.0195, :Al => -105.5954]);
+
+@show model.basis.BB
+
+model2 = acemodel(elements = [:Ti, :Al],
+                order = 2,
+                totaldegree = 6,
+                rcut = 5.5,
+                Eref = [:Ti => -1586.0195, :Al => -105.5954]);
+
+@show model2.basis.BB
